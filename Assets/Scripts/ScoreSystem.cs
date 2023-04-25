539a57184549f88dd2dda1b6a18ab2934732a655
nour ltaief <nerkolt0@gmail.com>
2023-04-25 15:26:37 +0100

    divinus scripts
    new scripts

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+public class ScoreSystem : MonoBehaviour
+{
+   [SerializeField] 
+   private TMP_Text scoreText;
+   public float score;
+   public TMP_Text finalscoreText;
+    public GameObject scoreCanvas;
+    private void Start()
+    {
+        DisplayScore();
+        ResetScore();
+
+    }
+    public void Scoring(float scoreRate)
+    {
+        score += scoreRate;
+        scoreText.text = "" + score;
+        PlayerPrefs.SetFloat("score",score + PlayerPrefs.GetFloat("score"));
+    }
+
+    public void DisplayScore()
+    {
+        Scene current = SceneManager.GetActiveScene();
+        if (current.name == "MainMenu" && PlayerPrefs.GetFloat("score") !=0)
+        {
+            //finalscoreText = Find("CanvasMain/Score/scoreResult").GetComponent<TextMeshPro>();
+            scoreCanvas.SetActive(true);
+            finalscoreText.text = "" + PlayerPrefs.GetFloat("score");
+        }
+        
+    }
+    public void ResetScore()
+    {
+        PlayerPrefs.SetFloat("score", 0f);
+    }
+
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textCounter;
+    public float timeRemaining ;
+    public bool fade;
+    public bool timer, countdown;
+    float timeCount;
+    bool minuteLeft;
+
+    private AudioManager audioManager;
+    private ScoreSystem sc;
+
+    private void Start() {
+
+        sc = FindObjectOfType<ScoreSystem>();
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+    void Update()
+    {
+        SwitchTimer(countdown, timer);
+        }
+        
+       void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        textCounter.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if(timeToDisplay < 40 && !minuteLeft){
+            minuteLeft = true;
+            textCounter.color = Color.red;
+            audioManager.Play("Clock");
+        }
+    }
+    void LoadOtherScene(float timeToDisplay)
+    {
+        if (timeToDisplay == 0 || sc.score==10)
+        {
+            StartCoroutine(GoToSceneRoutine());
+            audioManager.Stop("Clock");
+        }
+    }
+    
+    IEnumerator GoToSceneRoutine()
+    {
+        //ending condition : reset all
+        yield return new WaitForSeconds(3f);
+    }
+    public void SwitchTimer(bool countdown, bool timer)
+    {
+        //timeCount = 0;
+        switch (true)
+        {
+            case true when countdown:
+                DisplayTime(timeRemaining);
+                LoadOtherScene(timeRemaining);
+                if (timeRemaining > 0)
+                {
+                    timeRemaining -= Time.deltaTime;
+
+                }
+
+                else
+                {
+                    timeRemaining = 0;
+                }
+                break;
+            case true when timer:
+                timeCount += Time.deltaTime;
+                DisplayTime(timeCount);
+                LoadOtherScene(timeCount);
+                break;
+            default:
+                break;
+        }
+    }
+}
